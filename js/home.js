fb3155cc3bf79632e4fd04c75f0ab3d95e34ee0b
Gavin Pereira <gavineditz@gmail.com>
2021-02-16 17:30:49 +0530

    idk

--- js/home.js
+++ js/home.js
@@ -1,7 +1,6 @@
 bars = document.querySelector('header .bars');
 nav = document.querySelector('header .nav');
 // darks = document.querySelectorAll('.darken');
-body = document.querySelector('body');
 main = document.querySelector('main');
 
 bars.addEventListener('click', () => {
@@ -9,14 +8,12 @@ bars.addEventListener('click', () => {
     if (cond) {
         bars.src = 'img/icon-close.svg';
         main.style.filter = 'brightness(30%)';
-        body.style.overflow = 'hidden';
         // darks.forEach(dark => {
         //     dark.style.filter = 'brightness(30%)';
         // })
     } else {
         bars.src = 'img/icon-hamburger.svg';
         main.style.filter = 'none';
-        body.style.overflow = 'unset';
         // darks.forEach(dark => {
         //     dark.style.filter = 'none';
         // })
